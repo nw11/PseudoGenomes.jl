434190bcc3dad536d6895e2d1ab219950f82b56a
nw11 <nw11@sanger.ac.uk>
2015-03-05 13:51:32 +0000

    more descriptive name for argument in check_variant_alleles_in_fasta_file

--- src/pseudogenome-snp.jl
+++ src/pseudogenome-snp.jl
@@ -106,10 +106,10 @@ function check_variant_alleles(seq,snps::DataFrame)
     return (num_substituted,num_ref,num_other)
 end
 
-function check_variant_alleles_in_fasta_file(filename,vcf_filename; seq_id_type="ucsc" )
+function check_variant_alleles_in_fasta_file(fastafile,vcf_filename; seq_id_type="ucsc" )
     Lumberjack.info("Start reading VCF file")
     snp_df = read_snp_positions_from_columns(vcf_filename,seq_id_type)
-    fr = FastaReader{Vector{Char}}(filename)
+    fr = FastaReader{Vector{Char}}(fastafile)
     Lumberjack.info("Start checking snp substitution of fasta file")
     for (desc, seq) in fr
          seqlength = length(seq)
